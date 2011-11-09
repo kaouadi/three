cf1ce7de02ce08f3c3c363c07e149e874b8834a6
Karim Aouadi <kdolphinus@gmail.com>
2011-11-09 18:30:42 +0100

    Create table form

--- ruby-projects/three/app/controllers/quotes_controller.rb
+++ ruby-projects/three/app/controllers/quotes_controller.rb
@@ -2,6 +2,8 @@ class QuotesController < ApplicationController
   def new
     @quote = Quote.new(name: "Quote 1")
     
+   
+    
     4.times do |i|
       quote_line = @quote.quote_lines.build(quantity: i)
       quote_line.item = Item.new(description: "Item #{i}")
--- ruby-projects/three/public/javascripts/application.js
+++ ruby-projects/three/public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(function($) {
+	
+	$('form a.remove_fields').live('click', function() {
+	    var hidden_field = $(this).prev('input[type=hidden]')[0];
+	    if(hidden_field) {
+	      hidden_field.value = '1';
+	    }
+	    $(this).closest('.fields').hide();
+	    //$(this).closest("form").trigger('nested:fieldRemoved');
+	    return false;
+	  });	
+});
