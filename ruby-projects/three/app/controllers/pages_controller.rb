d05336150f5a402efb23be74615c1415d83c3ea0
Karim Aouadi <kdolphinus@gmail.com>
2011-12-04 17:36:27 +0100

    add method respond_to

--- ruby-projects/three/app/controllers/pages_controller.rb
+++ ruby-projects/three/app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  
+  #respond_to :html, :xml, :json, :js
   def new
     @status = "created"
   end
@@ -16,6 +16,21 @@ class PagesController < ApplicationController
   end
 
   def show
+    @quote = Quote.find_by_id(params[:id])
+    #respond_with(@quote)
+  end
+  
+  def index
+    @quotes = Quote.all
+    respond_to do |format|
+      format.html{render :json => @quotes}
+      format.xml{render :xml =>@quotes}
+      format.json{render :json =>@quotes}
+    end
+    #render :json => @quotes
+    #respond_with(@quotes) do |format|
+     # format.js {}
+    #end
   end
 
 end
